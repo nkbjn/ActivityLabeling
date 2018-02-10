93bb372211aa6f558b1337eafa1efc04e95e9a92
wataru-sa <sasaki.wataru.sl9@is.naist.jp>
2018-02-11 01:33:07 +0900

    [add]SetupViewControllerを追加

---
+++ ActivityLabeling/SetupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SetupViewController.swift
+//  ActivityLabeling
+//
+//  Created by Wataru Sasaki on 2018/02/11.
+//  Copyright © 2018年 Wataru Sasaki. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class SetupViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
