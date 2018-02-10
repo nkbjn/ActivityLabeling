62673fa0bc601997a613b2fa48b46b72199636fb
wataru-sa <sasaki.wataru.sl9@is.naist.jp>
2018-02-10 13:59:13 +0900

    [add]SettingViewControllerを追加

---
+++ ActivityLabeling/SettingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingViewController.swift
+//  ActivityLabeling
+//
+//  Created by Wataru Sasaki on 2018/02/10.
+//  Copyright © 2018年 Wataru Sasaki. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class SettingViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
