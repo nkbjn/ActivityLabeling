720db08408c5a63c51bd78a746a489ec4c1cc493
wataru-sa <sasaki.wataru.sl9@is.naist.jp>
2018-02-10 23:43:16 +0900

    [add]HistoryTableViewControllerを追加

---
+++ ActivityLabeling/HistoryTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  HistoryTableViewController.swift
+//  ActivityLabeling
+//
+//  Created by Wataru Sasaki on 2018/02/10.
+//  Copyright © 2018年 Wataru Sasaki. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
