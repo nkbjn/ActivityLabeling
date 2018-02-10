50e53ce3031de6979cf15f4d52ed38bd7f74cd8c
wataru-sa <sasaki.wataru.sl9@is.naist.jp>
2018-02-11 01:16:38 +0900

    [update]ラベリングの履歴を確認できるように更新

--- ActivityLabeling/HistoryTableViewController.swift
+++ ActivityLabeling/HistoryTableViewController.swift
@@ -7,22 +7,48 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HistoryTableViewController: UITableViewController {
+    
+    lazy var realm = try! Realm()
+    var labels: Results<Label>!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "履歴"
+        
+        labels = realm.objects(Label.self)
 
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return labels.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let label = labels[indexPath.row]
+        
+        cell.textLabel?.text = label.activities.reduce(""){(result, label) in
+            return result + label.activity
+        }
+        
+        let f = DateFormatter()
+        f.dateStyle = .medium
+        f.timeStyle = .medium
+        f.locale = Locale(identifier: "ja_JP")
+        cell.detailTextLabel?.text = f.string(from: label.time)
+        
+        return cell
     }
 
 }
