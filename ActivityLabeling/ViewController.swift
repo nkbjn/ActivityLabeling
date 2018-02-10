58a97014262099543278f7afdf6fe4f40a263627
wataru-sa <sasaki.wataru.sl9@is.naist.jp>
2018-02-10 11:14:32 +0900

    Initial Commit

---
+++ ActivityLabeling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ActivityLabeling
+//
+//  Created by Wataru Sasaki on 2018/02/10.
+//  Copyright © 2018年 Wataru Sasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
